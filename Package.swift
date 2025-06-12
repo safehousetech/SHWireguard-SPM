5d46d683bbff36d0daf48fae88a58e522eabe32e
AbhishekChoudhary4 <abhishek@safehousetech.com>
2025-06-13 00:02:47 +0530

    [UPDATE]:-Updated checksum

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
 		.binaryTarget(
 			name: "SHWireGuardKit",
 			url: "https://github.com/safehousetech/SHWireguard-SPM/releases/download/1.0/SHWireGuardKit.xcframework.zip",
-			checksum: "a779750fd65b74e466b6a6d98c30029632754f9a6f942e32993d331d5037f0f1"
+			checksum: "afd98f0fa9aec3a3a4a70a32e9a9ceeaeda44fd29c7244fb0572526cdce86874"
 		)
 	]
 )
